f9eba9a113ec32256017c04e4a1539a3478de375
d <d@kitchenlight.labor.koeln.ccc.de>
2012-04-26 22:41:13 +0200

    bild_scroll: gif-animation support

--- bild_scroll.py
+++ bild_scroll.py
@@ -1,7 +1,7 @@
 import led
 import time
 import math
-import Image
+import Image, ImageSequence
 import sys
 import os
 import fcntl
@@ -27,29 +27,30 @@ x=-1
 y=-1
 
 output=led.output()
-image=Image.open('bild.png')
-data = image.load()
-print image.size
+image=Image.open('bild.gif')
+
+#gif version
+frameList = map(lambda frame: frame.convert('RGB').load(), ImageSequence.Iterator(image))
+
+#png version
+#data = image.load()
+#frameList = [data]
+
+
 fd = sys.stdin.fileno()
 fl = fcntl.fcntl(fd, fcntl.F_GETFL)
 fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
 
 allpixel = reduce(lambda x,z: x+z, map(lambda y: map(lambda x: (x,y), range(30)), range(6)))
-offset = 0
 while True:
-	offset = offset % 30
-        for (x,y) in allpixel:
-		pt = tmp.getPixel(((x+offset)%30),y)
-		color=data[x,y]
-		pt.setColor(color[0]<<2,color[1]<<2,color[2]<<2)
-			
-	#output.write(leer)
-	#time.sleep(1)
-	output.write(tmp)
-	time.sleep(sl)
-	offset += 1
+	for frame in frameList:
+		for (x,y) in allpixel:
+			pt = tmp.getPixel(x,y)
+			color=frame[x,y]
+			pt.setColor(color[0]<<2,color[1]<<2,color[2]<<2)
+		output.write(tmp)
+		time.sleep(sl)
 	try:
 		sl = float(sys.stdin.read())
 	except IOError:
 		pass
-#map(lambda l: l.setColor(5,0,0), tmp.getAllPixel())
